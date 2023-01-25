81b0af7c25b9161ecccfdfbadc9df686c43862be
Mario <marchetti2@gmail.com>
2023-01-25 00:34:46 -0300

    path sqlite

--- Todoey/Controllers/TableListViewController.swift
+++ Todoey/Controllers/TableListViewController.swift
@@ -12,14 +12,13 @@ import CoreData
 class TableListViewController: UITableViewController {
     
     var todoArray = [Item]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-    
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         loadTodoData()
     }
     
